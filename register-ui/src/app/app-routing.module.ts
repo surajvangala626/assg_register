17c358826fc1b2ff0312a3b379000a63e0696889
surajvangala626 <sricharansimha.velpur@rubiconred.com>
2019-09-21 00:30:43 +0530

    Sample UI Pages

--- register-ui/src/app/app-routing.module.ts
+++ register-ui/src/app/app-routing.module.ts
@@ -1,11 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
-
-const routes: Routes = [];
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component'
+const routes: Routes = [{
+  path: '', children: [
+    { path: '', redirectTo: '/register', pathMatch: 'full' },
+    {
+      path: 'register', component: RegisterComponent
+    },
+    {
+      path: 'login', component: LoginComponent
+    }
+  ]
+}];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes, { scrollPositionRestoration: 'enabled' }),
+    CommonModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- register-ui/src/app/app.module.ts
+++ register-ui/src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    DashboardComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
